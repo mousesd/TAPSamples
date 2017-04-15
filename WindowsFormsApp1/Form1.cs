144c1bbfa99b07da424113458a09f303b86cb654
Lee SangDae <mousesd@gmail.com>
2017-04-15 13:40:48 +0900

    Task.Factory.StartNew() 메서드 사용의 위험성 예제

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -33,7 +33,7 @@ namespace WindowsFormsApp1
 
             //# 1.
             //# - SomeMethod()가 첫번째만 ThreadPool의 스레드를 사용, 이후 2, 3번째는 모두 UI 스레드를 사용하는 문제점
-            //# - 이 코드에 대한 개선사항은 아래 #2, #3 참조
+            //# - 이 코드에 대한 개선사항은 아래 #3, #4 참조
             var uiTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
             Task.Factory.StartNew(() => this.SomeMethod(counter))
                 .ContinueWith(task =>
@@ -42,7 +42,16 @@ namespace WindowsFormsApp1
                     this.Compute(counter - 1);
                 }, uiTaskScheduler);
 
-            //# 2.
+            //# 2.위 #1과 동일한 코드
+            //var uiTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+            //Task.Factory.StartNew(() => this.SomeMethod(counter), CancellationToken.None, TaskCreationOptions.None, TaskScheduler.Current)
+            //    .ContinueWith(task =>
+            //    {
+            //        textBox1.Text = task.Result.ToString();
+            //        this.Compute(counter - 1);
+            //    }, uiTaskScheduler);
+
+            //# 3.
             //var uiTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
             //Task.Factory.StartNew(() => this.SomeMethod(counter), CancellationToken.None, TaskCreationOptions.None, TaskScheduler.Default)
             //    .ContinueWith(task =>
@@ -51,7 +60,7 @@ namespace WindowsFormsApp1
             //        this.Compute(counter - 1);
             //    }, uiTaskScheduler);
 
-            //# 3.
+            //# 4.
             //var uiTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
             //Task.Run(() => this.SomeMethod(counter))
             //    .ContinueWith(task =>
